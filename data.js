a53aa1a52875b5335776884a3501095e2e8cceee
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-15 22:02:48 +0000

    make the event handler that will apply the new css to the car img for the correct arrow press. #2

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 /* exported data */
 
 var data = {
-  Orientation: '',
-  Location: ''
+  orientation: '',
+  location: ''
 };
--- main.js
+++ main.js
@@ -3,5 +3,21 @@
 var $car = document.querySelector('img');
 
 document.addEventListener('keydown', function (event) {
-  console.log(event);
+  switch (event.key) {
+    case 'ArrowUp':
+      $car.className = 'north';
+      data.orientation = 'north';
+      break;
+    case 'ArrowRight':
+      $car.className = 'east';
+      data.orientation = 'east';
+      break;
+    case 'ArrowDown':
+      $car.className = 'south';
+      data.orientation = 'south';
+      break;
+    case 'ArrowLeft':
+      $car.className = 'west';
+      data.orientation = 'west';
+  }
 });
